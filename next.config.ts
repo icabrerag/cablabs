e6d9829841830804b3d046133745e2a152949c5c
icabrerag <icabrera.g17@gmail.com>
2026-04-22 16:36:51 -0400

    config github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "cablabs";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
